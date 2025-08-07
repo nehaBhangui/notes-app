dd5c9b2abc4e3d2d2b1d6cfbcd942b602466af70
nehaBhangui <nehasinaibhangui@gmail.com>
2025-08-08 00:14:38 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import Homepage from './pages/Homepage'
+import NoteDetailsPage from './pages/NoteDetailsPage'
+import CreatePage from './pages/CreatePage'
+
+
+const App = () => {
+  return (
+    <div className='relative h-full w-full'>
+      <div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]' />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailsPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,7 @@
+export function formatdate(date) {
+    return date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+    });
+}
